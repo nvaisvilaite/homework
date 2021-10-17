4d88a8e406ced650e5b5ff65f2408cdc4b4b72cd
Neringa <n.vaisvilaite@gmail.com>
2021-10-17 21:04:09 +0300

    antra-pataisymai

--- antra uzd/script.js
+++ antra uzd/script.js
@@ -1,8 +1,16 @@
-var kaina = parseInt(prompt("Parašyk skaičių"));
-if(isNaN(kaina)) {
-  alert('Neteisingai');
+var persons = [];
+persons[1] = 'Jon Snow';
+persons[2] = 'Cersei Lannister';
+persons[3] = 'Daenerys Targaryen';
+persons[4] = 'Theon Greyjoy';
+persons[5] = 'Tyrion Lannister';
+persons[6] = 'Arya Stark';
+
+var number = parseInt(prompt('Įveskite skaičių nuo 1 iki 6.'));
+
+if(isNaN(number) || persons[number] == undefined) {
+	alert('Ne skaičius arba masyvas neturi tokio įrašo.');
 } else {
-  var pvm = kaina * 0.21;
-  var suma = kaina + pvm;
-  console.log("Kaina be pvm: " + kaina + "€" + ", kaina su pvm: " + suma);
+  alert('Rasta - peržiūrėti console');
+	console.log(persons[number]);
 }
